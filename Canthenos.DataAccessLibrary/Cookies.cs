d8040056e986835571943f4601895be3f78e21c7
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-04-02 15:17:32 +0200

    Added methods for reading and writing cookies from browser

---
+++ Canthenos.DataAccessLibrary/Cookies.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+
+namespace Canthenos.DataAccessLibrary;
+
+public class Cookies : ICookies
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public Cookies(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+    
+    public async Task WriteCookie(string name, string value, DateTime expires)
+    {
+        await _jsRuntime.InvokeAsync<object>("WriteCookie.WriteCookie", name, value, expires);
+    }
+
+    public async Task<string> ReadCookie(string name)
+    {
+        return await _jsRuntime.InvokeAsync<string>("ReadCookie.ReadCookie", name);
+    }
+}
