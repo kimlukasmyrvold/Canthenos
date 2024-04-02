1bdda41cb9caf203c29939e44164e0437706dcb1
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-04-02 16:31:38 +0200

    Component and methods for getting and viewing all users from database

---
+++ Canthenos.DataAccessLibrary/IUsersData.cs
@@ -0,0 +1,14 @@
+﻿using Canthenos.DataAccessLibrary.Models;
+
+namespace Canthenos.DataAccessLibrary;
+
+public interface IUsersData
+{
+    Task<List<UsersModel>> GetUsers(int roleId = 0);
+    Task<bool> UserExists(string username);
+    Task<(string? salt, string? hash)> UserPassword(string username);
+    Task<(bool success, string value)> CreateSessionToken(string username, string userAgent);
+    Task<bool> GetSessionToken(string token);
+    Task RevokeSessionToken(string token);
+    Task<bool> CheckSessionToken(string token);
+}
