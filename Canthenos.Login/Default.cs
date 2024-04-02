d034228d1494855db560f8f58f9e15162a178797
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-04-02 15:19:27 +0200

    File for storing default value related to login (stretchinterval and saltsize)

--- Canthenos.Login/Default.cs
+++ Canthenos.Login/Default.cs
@@ -1,6 +1,6 @@
 ﻿namespace Canthenos.Login;
 
-public abstract class Default
+public class Default
 {
     public static int SaltSize { get; set; } = 32;
     public static int StretchInterval { get; set; } = 1_000_000;
