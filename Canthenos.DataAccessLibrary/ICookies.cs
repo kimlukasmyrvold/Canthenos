6c4f9fd2def9544a3a4f006d28757ec590c36879
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-04-02 15:44:33 +0200

    Added methods for reading and writing cookies from browser

---
+++ Canthenos.DataAccessLibrary/ICookies.cs
@@ -0,0 +1,7 @@
+﻿namespace Canthenos.DataAccessLibrary;
+
+public interface ICookies
+{
+    Task WriteCookie(string name, string value, DateTime expires);
+    Task<string> ReadCookie(string name);
+}
