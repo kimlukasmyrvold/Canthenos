4b0931d7c3d0c40b70dc4a0d36f8abece1d2362e
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-04-02 17:05:44 +0200

    Change model for menus

--- Canthenos.DataAccessLibrary/Models/MenusModel.cs
+++ Canthenos.DataAccessLibrary/Models/MenusModel.cs
@@ -4,12 +4,23 @@ public class MenusModel
 {
     public int MenuId { get; set; }
     public string? MenuName { get; set; }
-    public int DishId { get; set; }
-    public string? DishName { get; set; }
-    public string? DishPrice { get; set; }
-    public string? DishImagePath { get; set; }
-    public int DishType { get; set; }
-    public string? DishTypeName { get; set; }
+    public string? MenuDescription { get; set; }
+    public string? MenuImagePath { get; set; }
+    public int ProductId { get; set; }
+    public int ProductTypeId { get; set; }
+    public string? ProductTypeName { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? ImagePath { get; set; }
+    public string? Price { get; set; }
     public int DayId { get; set; }
     public string? DayName { get; set; }
+    public string? AvailableFromTime { get; set; }
+    public string? AvailableToTime { get; set; }
+    public string? AvailableFromDate { get; set; }
+    public string? AvailableToDate { get; set; }
+    public string? CreatedAt { get; set; }
+    public string? UpdatedAt { get; set; }
+    public string? MenuCreatedAt { get; set; }
+    public string? MenuUpdatedAt { get; set; }
 }
