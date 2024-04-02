cc0fe6c617e27f9181b2dfdf3bc00c3e1e5e0692
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-04-02 17:02:16 +0200

    Components and methods for getting and viewing all dishes from database

---
+++ Canthenos.DataAccessLibrary/DishesData.cs
@@ -0,0 +1,28 @@
+﻿using Canthenos.DataAccessLibrary.Models;
+
+namespace Canthenos.DataAccessLibrary;
+
+public class DishesData : IDishesData
+{
+    private readonly ISqlDataAccess _db;
+
+    public DishesData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<List<DishesModel>> GetDishes(int id = 0)
+    {
+        var sql = id switch
+        {
+            0 => "select * from ViewDishes;",
+            1 => "select * from ViewDishes order by Price;",
+            2 => "select * from ViewDishes order by Price DESC;",
+            3 => "select * from ViewDishes order by Name;",
+            4 => "select * from ViewDishes order by Name DESC;",
+            _ => "select * from ViewDishes;"
+        };
+
+        return _db.LoadData<DishesModel, dynamic>(sql, new { });
+    }
+}
---
+++ Canthenos.DataAccessLibrary/IDishesData.cs
@@ -0,0 +1,8 @@
+﻿using Canthenos.DataAccessLibrary.Models;
+
+namespace Canthenos.DataAccessLibrary;
+
+public interface IDishesData
+{
+    Task<List<DishesModel>> GetDishes(int id = 0);
+}
