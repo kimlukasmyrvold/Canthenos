d4ca96735edba0dbcc7efcd7e58eefbebf02f5ca
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-04-02 16:19:03 +0200

    Component and methods for getting and viewing all snacks from database

---
+++ Canthenos.DataAccessLibrary/Models/SnacksModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Canthenos.DataAccessLibrary.Models;
+
+public class SnacksModel
+{
+    public int ProductId { get; set; }
+    public int ProductTypeId { get; set; }
+    public string? ProductTypeName { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? ImagePath { get; set; }
+    public string? Price { get; set; }
+    public string? AvailableFromTime { get; set; }
+    public string? AvailableToTime { get; set; }
+    public string? AvailableFromDate { get; set; }
+    public string? AvailableToDate { get; set; }
+    public string? CreatedAt { get; set; }
+    public string? UpdatedAt { get; set; }
+}
---
+++ Canthenos.DataAccessLibrary/SnacksData.cs
@@ -0,0 +1,28 @@
+﻿using Canthenos.DataAccessLibrary.Models;
+
+namespace Canthenos.DataAccessLibrary;
+
+public class SnacksData : ISnacksData
+{
+    private readonly ISqlDataAccess _db;
+
+    public SnacksData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<List<SnacksModel>> GetSnacks(int id = 0)
+    {
+        var sql = id switch
+        {
+            0 => "select * from ViewSnacks;",
+            1 => "select * from ViewSnacks order by Price;",
+            2 => "select * from ViewSnacks order by Price DESC;",
+            3 => "select * from ViewSnacks order by Name;",
+            4 => "select * from ViewSnacks order by Name DESC;",
+            _ => "select * from ViewSnacks;"
+        };
+
+        return _db.LoadData<SnacksModel, dynamic>(sql, new { });
+    }
+}
