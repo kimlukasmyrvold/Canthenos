e5e8db56973c22c240cbd8413b9731c34fc2de0f
kimlukasmyrvold <kimlukasmyrvold06@gmail.com>
2024-04-06 21:58:40 +0200

    Methods for sessionstorage

---
+++ Canthenos.DataAccessLibrary/ISessionStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace Canthenos.DataAccessLibrary;
+
+public interface ISessionStorage
+{
+    Task<string> Get(string key);
+    Task Set(string key, string value);
+    Task Remove(string key);
+    Task Clear();
+}
---
+++ Canthenos.DataAccessLibrary/SessionStorage.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.JSInterop;
+
+namespace Canthenos.DataAccessLibrary;
+
+public class SessionStorage : ISessionStorage
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public SessionStorage(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+    
+    public async Task<string> Get(string key)
+    {
+        return await _jsRuntime.InvokeAsync<string>("SessionStorage.get", key);
+    }
+    
+    public async Task Set(string key, string value)
+    {
+        await _jsRuntime.InvokeAsync<object>("SessionStorage.set", key, value);
+    }
+    
+    public async Task Remove(string key)
+    {
+        await _jsRuntime.InvokeAsync<object>("SessionStorage.remove", key);
+    }
+    
+    public async Task Clear()
+    {
+        await _jsRuntime.InvokeAsync<object>("SessionStorage.clear");
+    }
+    
+}
